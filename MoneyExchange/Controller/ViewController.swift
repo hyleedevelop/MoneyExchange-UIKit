b893ee0629680710b644d18421639e95d0576963
hyleedevelop <hyleedevelop@gmail.com>
2022-11-09 23:46:54 +0900

    Fill more detailed comments

--- MoneyExchange/Controller/ViewController.swift
+++ MoneyExchange/Controller/ViewController.swift
@@ -25,9 +25,8 @@ class ViewController: UIViewController {
     let toolbarFlexibleSpace = UIBarButtonItem(
         barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
     
-    var exchangeRateManager = ExchangeRateManager(rowNum: 22)
+    var exchangeRateManager = ExchangeRateManager(rowNum: 0)
     
-    var selectedRowNum: Int = 0
     var dateString: String = ""
     var currency1TypeString: String = ""
     var currency2TypeString: String = ""
@@ -35,69 +34,81 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /* ViewController에게 위임하기 */
         currency1Picker.delegate = self
         currency2Picker.delegate = self
         currency1Picker.dataSource = self
         currency2Picker.dataSource = self
         exchangeRateManager.delegate = self
         
+        /* 각 textfield를 눌렀을 때 보여줄 picker 생성 */
         currencyDate.inputView = currencyDatePicker
         currency1Type.inputView = currency1Picker
         currency2Type.inputView = currency2Picker
      
+        /* picker 위에 toolbar 추가 */
         toolbar.sizeToFit()
         toolbar.frame = CGRect(x: 0, y: 0, width: 0, height: 40)
         toolbar.setItems([toolbarFlexibleSpace, toolbarDoneButton], animated: true)
-        
+        currencyDate.inputAccessoryView = toolbar
+        currency1Type.inputAccessoryView = toolbar
+        currency2Type.inputAccessoryView = toolbar
         currency1Price.inputAccessoryView = toolbar
         currency2Price.inputAccessoryView = toolbar
-        currencyDate.inputAccessoryView = toolbar
+        
+        /* 날짜의 picker에 대한 설정 */
         currencyDatePicker.datePickerMode = .date
         currencyDatePicker.preferredDatePickerStyle = .wheels
         currencyDatePicker.addTarget(self, action: #selector(datePickerDone(_:)), for: .valueChanged)
         currencyDatePicker.locale = Locale(identifier: "ko_KR")
-        currency1Type.inputAccessoryView = toolbar
-        currency2Type.inputAccessoryView = toolbar
 
+        /* 기본 설정값으로 셋팅 */
         makeDefaultSetting()
     }
         
+    /* 기본 설정값으로 셋팅하는 함수 */
     func makeDefaultSetting() {
-        // textfield 및 picker의 기본값 설정: 통화 1번은 달러, 2번은 원(고정)
+        /* 날짜의 textfield에 대한 형식 지정 및 기본값 설정 */
         let todayDateFormatter = DateFormatter()
         todayDateFormatter.dateFormat = "yyyy년 MM월 dd일 (E)"
         todayDateFormatter.locale = Locale(identifier: "ko_KR")
         currencyDate.text = todayDateFormatter.string(from: Date())
-        currency1Type.text = exchangeRateManager.currencyArray[22]
-        currency2Type.text = exchangeRateManager.currencyArray[13]
+        
+        /* 통화 및 금액의 textfield에 대한 기본값 설정 */
+        currency1Type.text = exchangeRateManager.currencyArray[22]  // 달러
+        currency2Type.text = exchangeRateManager.currencyArray[13]  // 원
         currency1Price.text = ""
         currency2Price.text = "?"
-        currency1Picker.selectRow(22, inComponent: 0, animated: true)
-        currency2Picker.selectRow(13, inComponent: 0, animated: true)
+        
+        /* 통화의 picker에 대한 기본값 설정 */
+        currency1Picker.selectRow(22, inComponent: 0, animated: true)  // 달러
+        currency2Picker.selectRow(13, inComponent: 0, animated: true)  // 원
     }
     
+    /* 팝업 메세지 만드는 함수 */
     func makePopUpMessage(msg: String) {
-        // 팝업으로 보여주고자 하는 메세지 설정
+        /* 팝업으로 보여주고자 하는 메세지 설정 */
         let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
         
-        // 현재 ViewController에서 팝업 실행
+        /* 현재 ViewController에서 팝업 실행 */
         self.present(alert, animated: true, completion: nil)
         
-        // 팝업이 withTimeInterval(초)만큼 유지되다가 사라짐
-        Timer.scheduledTimer(withTimeInterval: 1.2, repeats: false, block: { _ in alert.dismiss(animated: true, completion: nil)})
+        /* 팝업이 withTimeInterval(초)만큼 유지되다가 사라짐 */
+        Timer.scheduledTimer(withTimeInterval: 1.2, repeats: false,
+                             block: { _ in alert.dismiss(animated: true, completion: nil)})
     }
     
-    // 통화 및 날짜 선택 picker의 키보드 Close 버튼
+    /* 통화 및 날짜 선택 picker의 키보드 Close 버튼 */
     @objc func pickerClose() {
         self.view.endEditing(true)
     }
     
-    // 통화 선택 picker의 키보드 Done 버튼
+    /* 통화 선택 picker의 키보드 Done 버튼 */
     @objc func pickerDone() {
         self.view.endEditing(true)
     }
     
-    // 날짜 선택 picker의 키보드 Done 버튼
+    /* 날짜 선택 picker의 키보드 Done 버튼 */
     @objc func datePickerDone(_ sender: UIDatePicker) {
         let pickedDate = sender.date
 
@@ -111,25 +122,30 @@ class ViewController: UIViewController {
         self.dateString = pickedDateFormatter2.string(from: pickedDate)
     }
     
+    /* background를 터치했을 때 */
     @IBAction func tapBackgroundView(_ sender: Any) {
-        view.endEditing(true)
+        view.endEditing(true)  // 키보드 및 picker가 사라지도록 설정
     }
     
+    /* 통화1 종류 textfield를 눌렀을 때 */
     @IBAction func currency1TypePressed(_ sender: UITextField) {
     }
     
+    /* 통화2 종류 textfield를 눌렀을 때 */
     @IBAction func currency2TypePressed(_ sender: UITextField) {
-        currency2Type.endEditing(true)
+        currency2Type.endEditing(true)  // 내용을 수정하지 못하도록 설정
     }
     
+    /* 통화1 금액 textfield를 눌렀을 때 */
     @IBAction func currency1PricePressed(_ sender: UITextField) {
     }
     
+    /* 통화2 금액 textfield를 눌렀을 때 */
     @IBAction func currency2PricePressed(_ sender: UITextField) {
-        currency2Price.endEditing(true)
+        currency2Price.endEditing(true)  // 내용을 수정하지 못하도록 설정
     }
     
-    // 조회 버튼을 클릭했을 때
+    /* 조회 버튼을 클릭했을 때 */
     @IBAction func fetchButtonPressed(_ sender: UIButton) {
         if currency1Type.text != "" && currency1Price.text != "" {
             exchangeRateManager.row = currency1Picker.selectedRow(inComponent: 0)
@@ -140,7 +156,7 @@ class ViewController: UIViewController {
         }
     }
     
-    // 초기화 버튼을 클릭했을 때
+    /* 초기화 버튼을 클릭했을 때 */
     @IBAction func initializeButtonPressed(_ sender: UIButton) {
         makeDefaultSetting()
         makePopUpMessage(msg: "초기화 완료")
@@ -214,7 +230,6 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.selectedRowNum = row
         let selectedCurrency = exchangeRateManager.currencyArray[row]
         
         if pickerView == currency1Picker {
